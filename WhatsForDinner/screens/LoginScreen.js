b4f097cd042b367714b285c97c01da20d421e454
Zander <kruger.zander@icloud.com>
2019-03-09 13:13:46 +0200

    Removed some of the default stuff from the main tab screen, Added the login screen, Changed the default app screen to the login screen, Added random webstorm stuff to the repo

---
+++ WhatsForDinner/screens/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+export default class LoginScreen extends React.Component {
+    static navigationOptions = {
+        header: null,
+    };
+
+
+    render() {
+        return (<Text>Hello World!</Text>);
+    }
+}
